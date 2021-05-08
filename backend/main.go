d84844047e603edf86a85089427832b7f0c20522
jcleow <jc.leow94@gmail.com>
2021-05-08 19:46:17 +0800

    update websocket pkg

--- backend/main.go
+++ backend/main.go
@@ -6,63 +6,29 @@ import (
 	"github.com/jcleow/go-chat/pkg/websocket"
 )
 
-// // Defining an upgrader
-// // this will require a read and write buffer size
-// var upgrader = websocket.Upgrader{
-// 	ReadBufferSize: 1024,
-// 	WriteBufferSize: 1024,
-
-// 	// We'll need to check the origin of our connection
-//   	// this will allow us to make requests from our React
-//   	// development server to here.
-//   	// For now, we'll do no checking and just allow any connection
-// 	CheckOrigin: func(r *http.Request) bool{ return true},
-// }
-
-// // Defining a reader which will listen for new messages
-// // being sent to our WebSocket endpoint
-// func reader(conn *websocket.Conn){
-// 	for{
-// 	// read in a message
-// 	// p is []byte and messageType is an int with 
-// 	// value websocket.BinaryMessage or websocket.TextMessage
-// 	// https://pkg.go.dev/github.com/gorilla/websocket
-// 		messageType, p, err := conn.ReadMessage()
-// 		if err != nil{
-// 			log.Println(err)
-// 			return
-// 		}
-// 	// print out that message for clarity
-// 		fmt.Println(string(p))
-
-// 		if err := conn.WriteMessage(messageType, p); err != nil{
-// 			log.Println(err)
-// 			return
-// 		}
-// 	}
-// }
-
 // Define websocket endpoint
 func serveWs(w http.ResponseWriter, r *http.Request){
-	fmt.Println(r.Host)
+	fmt.Println("WebSocket endpoint hit")
 
 	//upgrade connection to a websocket connection
 	//https://stackoverflow.com/questions/50204967/what-is-websocket-upgrader-exactly
-	ws,err := websocket.Upgrade(w, r, nil)
+	conn,err := websocket.Upgrade(w, r)
 	if err != nil{
 		fmt.Fprintf(w,"%+V\n",err)
 	}
-	// listen indefinitely for new messages coming
-	// through on our WebSocket connection
-		// reader(ws)
-	go websocket.Writer(ws)
-	websocket.Reader(ws)
 
+	pool.Register <- client
+	client.Read()
 }
 
 
-func setupRoutes(){
-	http.HandleFunc("/ws",serveWs)
+func setupRoutes(){	
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws",func(w http.ResponseWriter, r *http.Request){
+		serveWs(pool, w, r)
+	})
 }
 
 func main(){
