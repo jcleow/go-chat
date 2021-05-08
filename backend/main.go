47814878af04f070aefb1f69a55c3788ba057523
jcleow <jc.leow94@gmail.com>
2021-05-08 18:58:28 +0800

    fixing websocket package bug

--- backend/main.go
+++ backend/main.go
@@ -2,46 +2,45 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"github.com/gorilla/websocket"
+	"github.com/jcleow/go-chat/pkg/websocket"
 )
 
-// Defining an upgrader
-// this will require a read and write buffer size
-var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024,
-	WriteBufferSize: 1024,
+// // Defining an upgrader
+// // this will require a read and write buffer size
+// var upgrader = websocket.Upgrader{
+// 	ReadBufferSize: 1024,
+// 	WriteBufferSize: 1024,
 
-	// We'll need to check the origin of our connection
-  	// this will allow us to make requests from our React
-  	// development server to here.
-  	// For now, we'll do no checking and just allow any connection
-	CheckOrigin: func(r *http.Request) bool{ return true},
-}
+// 	// We'll need to check the origin of our connection
+//   	// this will allow us to make requests from our React
+//   	// development server to here.
+//   	// For now, we'll do no checking and just allow any connection
+// 	CheckOrigin: func(r *http.Request) bool{ return true},
+// }
 
-// Defining a reader which will listen for new messages
-// being sent to our WebSocket endpoint
-func reader(conn *websocket.Conn){
-	for{
-	// read in a message
-	// p is []byte and messageType is an int with 
-	// value websocket.BinaryMessage or websocket.TextMessage
-	// https://pkg.go.dev/github.com/gorilla/websocket
-		messageType, p, err := conn.ReadMessage()
-		if err != nil{
-			log.Println(err)
-			return
-		}
-	// print out that message for clarity
-		fmt.Println(string(p))
+// // Defining a reader which will listen for new messages
+// // being sent to our WebSocket endpoint
+// func reader(conn *websocket.Conn){
+// 	for{
+// 	// read in a message
+// 	// p is []byte and messageType is an int with 
+// 	// value websocket.BinaryMessage or websocket.TextMessage
+// 	// https://pkg.go.dev/github.com/gorilla/websocket
+// 		messageType, p, err := conn.ReadMessage()
+// 		if err != nil{
+// 			log.Println(err)
+// 			return
+// 		}
+// 	// print out that message for clarity
+// 		fmt.Println(string(p))
 
-		if err := conn.WriteMessage(messageType, p); err != nil{
-			log.Println(err)
-			return
-		}
-	}
-}
+// 		if err := conn.WriteMessage(messageType, p); err != nil{
+// 			log.Println(err)
+// 			return
+// 		}
+// 	}
+// }
 
 // Define websocket endpoint
 func serveWs(w http.ResponseWriter, r *http.Request){
@@ -49,21 +48,20 @@ func serveWs(w http.ResponseWriter, r *http.Request){
 
 	//upgrade connection to a websocket connection
 	//https://stackoverflow.com/questions/50204967/what-is-websocket-upgrader-exactly
-	ws,err := upgrader.Upgrade(w, r, nil)
+	ws,err := websocket.Upgrade(w, r, nil)
 	if err != nil{
-		log.Println(err)
+		fmt.Fprintf(w,"%+V\n",err)
 	}
 	// listen indefinitely for new messages coming
 	// through on our WebSocket connection
-		reader(ws)
+		// reader(ws)
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
+
 }
 
 
 func setupRoutes(){
-	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Simple Server")
-	})
-	// map our `/ws` endpoint to the `serveWs` function
 	http.HandleFunc("/ws",serveWs)
 }
 
--- backend/pkg/websocket/websocket.go
+++ backend/pkg/websocket/websocket.go
@@ -14,7 +14,7 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request)bool{return true},
 }
 
-func Upgrade(w http.ResponseWriter, r *http.Request)(*websocket.Conn,error){
+func Upgrade(w http.ResponseWriter, r *http.Request) (*websocket.Conn,error){
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil{
 		log.Println(err)
